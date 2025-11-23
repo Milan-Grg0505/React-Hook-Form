07d3725ae8de6d674d1ed88c406ad58b3d5ceed9
Milan-Grg0505 <gurungmilan676@gmail.com>
2025-11-23 22:40:49 +0545

    Added others fields like gender,hobbies,bio, country and validate with yum

--- frontend/src/FormWithYup.jsx
+++ frontend/src/FormWithYup.jsx
@@ -17,8 +17,24 @@ const FormWithYup = () => {
     // age:yup.number().integer().min(18).max(80).required("Age is requrired"),
     password: yup.string().min(8).max(15)
       .required('Password is required'),
-    confirmPassword:yup.string().oneOf([yup.ref("password")],"Password doesnt match")
-    .required("confirm Password is required")
+    confirmPassword: yup.string().oneOf([yup.ref("password")], "Password doesnt match")
+      .required("confirm Password is required"),
+
+    gender: yup.string()
+      .oneOf(["male", "female", "others"], "Please select valid genders").
+      required("Please select your gender"),
+
+    hobbies: yup.array()
+      .of(yup.string()) //defines the schema or types for each array
+      .min(1, "Please select at least one hobby"),
+
+    countries: yup.string().min(1, "Select at least one country"),
+
+    bio:yup
+    .string()
+    .min(10,"Must be at least 10 characters")
+    .max(200,"Cannot exceed 200 characters")
+
   });
 
 
@@ -28,7 +44,11 @@ const FormWithYup = () => {
     reset,
     formState: { errors, isSubmitting }
   } = useForm({
-    resolver: yupResolver(schema)
+    resolver: yupResolver(schema),
+    defaultValues: {
+      hobbies: [],
+
+    }
   });
 
 
@@ -52,7 +72,7 @@ const FormWithYup = () => {
 
   return (
     <>
-      <div className='min-h-screen bg-gray-50 flex justify-center items-center'>
+      <div className='min-h-screen bg-gray-50 flex justify-center items-center py-6'>
 
         <div className='max-w-md w-full'>
           <form
@@ -69,7 +89,7 @@ const FormWithYup = () => {
                 {...register("firstName")}
                 type="text"
                 placeholder='First Name'
-                className={`w-full border border-gray-300 rouded-lg px-3 py-2 ${errors.firstName ? "border-red-500" : "border-gray-300"}`}
+                className={`w-full border border-gray-300 rounded-lg px-3 py-2 ${errors.firstName ? "border-red-500" : "border-gray-300"}`}
               />
               {errors.firstName && (
                 <p className='text-red-600'>{errors.firstName.message}</p>
@@ -88,7 +108,7 @@ const FormWithYup = () => {
                 }
                 type="text"
                 placeholder='Last Name'
-                className={`w-full border border-gray-300 rouded-lg px-3 py-2 ouline-0 ${errors.lastName ? "border-red-500" : "border-gray-300"}`}
+                className={`w-full border border-gray-300 rounded-lg px-3 py-2 outline-0 ${errors.lastName ? "border-red-500" : "border-gray-300"}`}
               />
 
               {errors.lastName && (
@@ -107,7 +127,7 @@ const FormWithYup = () => {
                 {...register("email")}
                 type="email"
                 placeholder='someone@gmail.com'
-                className={`w-full border border-gray-300 rouded-lg px-3 py-2 ouline-0 ${errors.lastName ? "border-red-500" : "border-gray-300"}`}
+                className={`w-full border border-gray-300 rounded-lg px-3 py-2 outline-0 ${errors.lastName ? "border-red-500" : "border-gray-300"}`}
 
               />
               {errors.email && (
@@ -127,7 +147,7 @@ const FormWithYup = () => {
                 {...register("password")}
                 type="password"
                 placeholder='Enter password'
-                className={`w-full border border-gray-300 rouded-lg px-3 py-2 ouline-0 ${errors.password ? "border-red-500" : "border-gray-300"}`}
+                className={`w-full border border-gray-300 rounded-lg px-3 py-2 outline-0 ${errors.password ? "border-red-500" : "border-gray-300"}`}
 
               />
               {errors.password && (
@@ -140,7 +160,7 @@ const FormWithYup = () => {
 
             <div className='w-full'>
               <label htmlFor="confirmPassword"
-              className='block text-md font-medium text-gray-700 mb-2'>
+                className='block text-md font-medium text-gray-700 mb-2'>
                 Confirm Password
               </label>
 
@@ -148,17 +168,122 @@ const FormWithYup = () => {
                 {...register("confirmPassword")}
                 type="password"
                 placeholder='Enter ConfirmPassword'
-                className={`w-full border border-gray-300 rouded-lg px-3 py-2 ouline-0 ${errors.confirmPassword ? "border-red-500" : "border-gray-300"}`}
+                className={`w-full border border-gray-300 rounded-lg px-3 py-2 outline-0 ${errors.confirmPassword ? "border-red-500" : "border-gray-300"}`}
               />
 
-               {errors.confirmPassword && (
+              {errors.confirmPassword && (
                 <p className='text-red-600'>{errors.confirmPassword.message}</p>
               )}
 
             </div>
 
-            
 
+            {/*  Gender Section*/}
+            <div className='w-full'>
+              <label htmlFor="gender" className='block text-md text-gray-700 mb-2 font-medium'>Gender</label>
+
+              <div className='flex gap-2 items-center text-md'>
+                <input type="radio" {...register("gender")} value="male" /> Male
+                <input type="radio" {...register("gender")} value="female" /> Female
+                <input type="radio" {...register("gender")} value="others" /> Others
+
+              </div>
+
+              {errors.gender && (
+                <p className='text-red-600'>{errors.gender.message}</p>
+              )}
+            </div>
+
+
+            {/* hobby field */}
+            <div className='w-full'>
+
+              <label htmlFor="hobbies" className='block text-md text-gray-700 mb-2 font-medium'>Hobbies</label>
+
+              <div className='flex gap-2 item-center flex-wrap text-md'>
+
+                <input
+                  {...register("hobbies")}
+                  type="checkbox"
+                  value="reading"
+                />Reading
+
+                <input
+                  {...register("hobbies")}
+                  type="checkbox"
+                  value="sports"
+                />Sports
+
+                <input
+                  {...register("hobbies")}
+                  type="checkbox"
+                  value="music"
+                />Music
+
+                <input
+                  {...register("hobbies")}
+                  type="checkbox"
+                  value="travel"
+                />Travel
+
+                <input
+                  {...register("hobbies")}
+                  type="checkbox"
+                  value="gaming"
+                />Gaming
+
+              </div>
+
+              {errors.hobbies && (
+                <p className='text-red-600'>{errors.hobbies.message}</p>
+              )}
+
+            </div>
+
+            {/* country field */}
+            <div className='w-full'>
+              <label htmlFor="countries"
+                className='text-md text-gray-700 mb-2 font-medium'>
+                Country
+              </label>
+
+              <select
+                {...register("countries")}
+                className={`w-full border rounded-lg px-3 py-2 focus:outline-none ${errors.countries ? "border-red-600" : "border-gray-300"}`}
+              >
+
+                <option value="">Select a country:</option>
+                <option value="nepal">Nepal</option>
+                <option value="usa">USA</option>
+                <option value="china">China</option>
+                <option value="canada">Canada</option>
+                <option value="korea">Korea</option>
+
+              </select>
+              {errors.countries && (
+                <p className='text-red-600'>{errors.countries.message}</p>
+              )}
+
+            </div>
+
+            {/*  Bio field*/}
+
+            <div className='w-full'>
+              <label htmlFor="bio"
+                className='text-md font-medium text-gray-700 mb-2'>
+                Bio
+              </label>
+
+              <textarea
+                {...register("bio")}
+                rows={5}
+                placeholder='Tell us about yourself...'
+                className={`w-full border rounded-lg px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500 resize-none ${errors.bio ? "border-red-500" : "border-gray-300"}`}
+              ></textarea>
+            {errors.bio && (
+              <p className='text-red-600'>{errors.bio.message}</p>
+            )}
+            </div>
 
 
             {/* button to submit form */}
