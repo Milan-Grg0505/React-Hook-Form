cfa267006d5bfce403bf0ffbe1c684e70cb7cb4e
Milan-Grg0505 <gurungmilan676@gmail.com>
2025-11-24 13:23:00 +0545

    show form with yup library

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,9 +8,9 @@ const App = () => {
 
   return (
     <>
-    <FormWithReactHook />
+    {/* <FormWithReactHook /> */}
     {/* <SimpleForm /> */}
-    {/* <FormWithYup /> */}
+    <FormWithYup />
     </>
   )
 }
